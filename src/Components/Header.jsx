83637d52a032a517d53ac22cc6d48af79db1ec0c
Narasimha Prasad <ynprasadkklreddy2@gmail.com>
2021-07-21 13:41:01 +0530

    done

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { MdHighlight } from "react-icons/md";
+
+function Header() {
+    return (
+        <header>
+            <h1>
+                <MdHighlight />
+                Keeper
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
